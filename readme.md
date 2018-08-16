8abe18f985a51dad0f8caec4ef8adabf867c7e6a
Alvin Hou <cryolite@ntu.im>
2018-08-16 17:28:51 +0800

    added readme.md and updates script(blur effect)

---
+++ readme.md
@@ -0,0 +1,17 @@
+# KMF Unblock Extension
+
+解除『考滿分』區域限制的擴充工具
+
+A Chrome and Firefox extension that bypasses KMF region restriction.
+
+## Links
+[Chrome Extension](https://chrome.google.com/webstore/detail/kmf-unblock/jlbknbfjajehjmfjegefddgegipadonb)
+
+[Firefox Extension](https://addons.mozilla.org/en-GB/firefox/addon/kmf-unblock/)
+
+## Screenshots
+> Region restriction
+![](https://i.imgur.com/dCHMA7H.png)
+
+> Extension installed
+![](https://i.imgur.com/pM1F5w8.png)
--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 window.onload = function () {
     /* remove blocking box */
-    document.querySelectorAll('.shield-box').forEach(s => s.remove())
-    /* remove blur effect after loaded */
-    setTimeout(function(){
-        document.querySelectorAll('.blur').forEach(b => b.classList.remove('blur'))
-    }, 777);
-};
-
+    document.querySelectorAll('.shield-box').forEach(s => s.remove());
+    /* remove blur effect */
+    (() => {
+        let times = 4;
+        (function run () {
+            document.querySelectorAll('.blur').forEach(b => b.classList.remove('blur'));
+            if(--times){
+                setTimeout(run, 500);
+            }
+        })();
+    })();
+}();
