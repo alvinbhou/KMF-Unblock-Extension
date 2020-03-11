ee7f889641c4875de9836b167d3bb9dbdd800c79
Alvin Hou <alvinhou@stanford.edu>
2020-03-11 16:08:58 -0700

    Remove delay before removing blur effects; Fix remove login block feature

--- script.js
+++ script.js
@@ -1,21 +1,33 @@
-window.onload = function() {
+window.onload = function () {
     /* TOEFL section */
     /* Remove blocking box */
     document.querySelectorAll('.shield-box').forEach(s => s.remove());
-    document.querySelectorAll('.login-cont').forEach(s => s.remove());
-    /* Remove blur effect */
-    (() => {
-        let times = 10;
-        (function run() {
+
+    /*  Select the nodes that to remove the blur and login block */
+    const targetNodes = document.querySelectorAll('.questions-tools, .practice-container');
+    console.log(targetNodes);
+    for (let targetNode of targetNodes) {
+        // Options for the observer (which mutations to observe)
+        const config = {
+            attributes: true,
+            childList: true,
+            subtree: true
+        };
+
+        // Callback function to execute when mutations are observed
+        const callback = function (e) {
+            document.querySelectorAll('.login-cont').forEach(s => s.remove());
             document.querySelectorAll('.blur').forEach(b => {
-                b.classList.remove('blur');
-                times = 0;
+                b.classList.remove('blur')
             });
-            if (times--) {
-                setTimeout(run, 200);
-            }
-        })();
-    })();
+        };
+
+        // Create an observer instance linked to the callback function
+        const observer = new MutationObserver(callback);
+
+        // Start observing the target node for configured mutations
+        observer.observe(targetNode, config);
+    }
 
     /* GMAT section */
     /* Remove blocking box and text for single practice questions*/
